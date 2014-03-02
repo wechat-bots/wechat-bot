148e7c2907406fa9c5d8823fb39df8879dfcaba3
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-02 17:25:40 +0800

    Add more test cases

--- test/index.js
+++ test/index.js
@@ -1,5 +1,7 @@
-var expect = require('chai').expect,
-    wechatHandler = require('..');
+var chai = require('chai');
+var expect = chai.expect;
+
+var wechatHandler = require('..');
 
 describe('wechat-handler', function() {
   var app = wechatHandler();
@@ -10,5 +12,22 @@ describe('wechat-handler', function() {
   it('should merge proto', function () {
     expect(app.handle).to.be.a('function');
     expect(app.use).to.be.a('function');
+  });
+
+  it('should call handlers', function () {
+    var foo = {};
+    var fn1 = function fn1(req, res, next) {
+      expect(req).to.equal(foo);
+      req.afterFn1 = true;
+      next();
+    };
+    var fn2 = function fn2(req, res) {
+      expect(req.afterFn1).to.be(true);
+      done();
+    };
+    app.use(fn1)
+      .use(fn2);
+
+    app.handle(foo);
   });
 });
