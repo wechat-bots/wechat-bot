c7dd89a9850cab6c9d10daafd38d84cbd296ac8b
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-02 16:59:53 +0800

    Add test

--- test/index.js
+++ test/index.js
@@ -2,8 +2,13 @@ var expect = require('chai').expect,
     wechatHandler = require('..');
 
 describe('wechat-handler', function() {
-  it('should say hello', function(done) {
-    expect(wechatHandler()).to.equal('Hello, world');
-    done();
+  var app = wechatHandler();
+  it('should have properties', function() {
+    expect(app.route).to.be.an('object');
+    expect(app.stack).to.be.an('array');
+  });
+  it('should merge proto', function () {
+    expect(app.handle).to.be.a('function');
+    expect(app.use).to.be.a('function');
   });
 });
