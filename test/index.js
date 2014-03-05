654f138969904d572a9314ebff7c477771e83cf6
Rogerz Zhang <rogerz.zhang@gmail.com>
2014-03-05 21:50:08 +0800

    Add default error handler

--- test/index.js
+++ test/index.js
@@ -3,8 +3,23 @@ var expect = chai.expect;
 
 var wechatHandler = require('..');
 
+var lastReply;
+
+function logLast(reply) {
+  lastReply = reply;
+}
+
 describe('wechat-handler', function() {
-  var handler = wechatHandler();
+  var handler;
+  var request = {};
+  var response = {
+    reply: logLast
+  };
+
+  beforeEach(function () {
+    handler = wechatHandler();
+  });
+
   it('should have properties', function() {
     expect(handler.route).to.be.an('object');
     expect(handler.stack).to.be.an('array');
@@ -14,21 +29,28 @@ describe('wechat-handler', function() {
     expect(handler.use).to.be.a('function');
   });
 
-  it('should call handlers', function () {
-    var foo = {};
+  it('should call handlers', function (done) {
     var fn1 = function fn1(req, res, next) {
-      expect(req).to.equal(foo);
+      expect(req).to.equal(request);
       req.afterFn1 = true;
       next();
     };
+    /* jshint expr: true */
     var fn2 = function fn2(req, res) {
-      expect(req.afterFn1).to.be(true);
+      expect(req.afterFn1).to.be.true;
       done();
     };
     handler.use(fn1)
       .use(fn2);
 
-    handler.handle(foo);
+    handler.handle(request, response);
   });
 
+  it('should reply pardon', function (done) {
+    response.reply = function (reply) {
+      expect(reply).to.eql({type: 'text', content: 'pardon'});
+      done();
+    };
+    handler.handle(request, response);
+  });
 });
